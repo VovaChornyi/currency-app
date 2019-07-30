8cdeffb12ff87f2bebf465d24913b113f117d5da
Volodymyr, Chornyi <volodymyr.chornyi@orilinc.com>
2019-07-30 14:53:05 +0300

    #2 Added custom Counter component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Counter from './Counter'
 
-const element = <h1>Hello World</h1>;
-
-ReactDOM.render(element, document.getElementById('root'));
+ReactDOM.render(<Counter/>, document.getElementById('root'));
