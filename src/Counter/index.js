2d24cbdf5f2a0ce6834c83e447ccb5a5400918a9
Volodymyr, Chornyi <volodymyr.chornyi@orilinc.com>
2019-07-30 16:28:03 +0300

    #3 Added button and Prettier plugin

--- src/Counter/index.js
+++ src/Counter/index.js
@@ -1,9 +1,19 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
 export default class Counter extends Component {
-    state = {value: 1};
+  state = { value: 1 };
 
-    render() {
-        return <div>{this.state.value}</div>
-    }
+  incrementValue() {
+    console.log("clicked");
+    this.setState({ value: this.state.value + 1 });
+  }
+
+  render() {
+    return (
+      <>
+        <div>{this.state.value}</div>
+        <button onClick={() => this.incrementValue()}> Increment </button>
+      </>
+    );
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Counter from './Counter'
+import React from "react";
+import ReactDOM from "react-dom";
+import Counter from "./Counter";
 
-ReactDOM.render(<Counter/>, document.getElementById('root'));
+ReactDOM.render(<Counter />, document.getElementById("root"));
