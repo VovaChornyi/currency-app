193b2d6c2907aa4538c7cb989be701d1979c8aa1
Volodymyr, Chornyi <volodymyr.chornyi@orilinc.com>
2019-07-31 11:44:13 +0300

    #4 Added ToolBar and Content

---
+++ src/App/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import ToolBar from "../ToolBar";
+import Content from "../Content";
+
+export default class App extends Component {
+  render() {
+    return (
+      <>
+        <ToolBar />
+        <Content />
+      </>
+    );
+  }
+}
---
+++ src/ToolBar/index.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class ToolBar extends Component {
+  render() {
+    return <div>TO DO</div>;
+  }
+}
