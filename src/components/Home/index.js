581e93efecf1ffd91716c2bf4043f54373a660fc
Volodymyr, Chornyi <volodymyr.chornyi@orilinc.com>
2019-08-01 13:13:49 +0300

    #6 Fixed Carousel - Decomposition

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,58 +3,33 @@ import Carousel from "react-bootstrap/Carousel";
 import img1 from "../../images/1.png";
 import img2 from "../../images/2.jpg";
 import img3 from "../../images/3.jpg";
+import CarouselItem from "./CarouselItem";
+import "./style.css";
 
 export default class Home extends Component {
-  render() {
-    const imgStyle = {
-      width: "640px",
-      height: "640px"
-    };
+  carouselImage = image => {
+    return <img className="d-block w-100" src={image} alt="First slide" />;
+  };
 
+  carouselItem = item => {
     return (
-      <Carousel>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src={img1}
-            alt="First slide"
-            style={imgStyle}
-          />
-
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src={img2}
-            alt="Third slide"
-            style={imgStyle}
-          />
-
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src={img3}
-            alt="Third slide"
-            style={imgStyle}
-          />
-
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>Praesent commodo cursus magna,</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
+      <Carousel.Item>
+        {this.carouselImage(item.image)}
+        <Carousel.Caption>
+          <h3>{item.label}</h3>
+          <p>{item.description} </p>
+        </Carousel.Caption>
+      </Carousel.Item>
     );
+  };
+
+  render() {
+    const items = [
+      new CarouselItem(img1, "Label1", "Desc1"),
+      new CarouselItem(img2, "Label2", "Desc2"),
+      new CarouselItem(img3, "Label3", "Desc3")
+    ];
+
+    return <Carousel>{items.map(this.carouselItem)}</Carousel>;
   }
 }
--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,7 +1,110 @@
 import React, { Component } from "react";
 
 export default class Table extends Component {
+  state = {
+    currencies: [
+      {
+        currency_code: "AUD",
+        buying_rate: "4.544384",
+        selling_rate: "4.571732",
+        unit_value: 1,
+        median_rate: "4.558058"
+      },
+      {
+        currency_code: "CAD",
+        buying_rate: "5.006467",
+        selling_rate: "5.036597",
+        unit_value: 1,
+        median_rate: "5.021532"
+      },
+      {
+        currency_code: "CZK",
+        buying_rate: "0.286544",
+        selling_rate: "0.288268",
+        unit_value: 1,
+        median_rate: "0.287406"
+      },
+      {
+        currency_code: "DKK",
+        buying_rate: "0.984556",
+        selling_rate: "0.990482",
+        unit_value: 1,
+        median_rate: "0.987519"
+      },
+      {
+        currency_code: "HUF",
+        buying_rate: "2.244031",
+        selling_rate: "2.257535",
+        unit_value: 100,
+        median_rate: "2.250783"
+      },
+      {
+        currency_code: "JPY",
+        buying_rate: "6.075705",
+        selling_rate: "6.112269",
+        unit_value: 100,
+        median_rate: "6.093987"
+      },
+      {
+        currency_code: "NOK",
+        buying_rate: "0.755141",
+        selling_rate: "0.759685",
+        unit_value: 1,
+        median_rate: "0.757413"
+      },
+      {
+        currency_code: "SEK",
+        buying_rate: "0.691130",
+        selling_rate: "0.695290",
+        unit_value: 1,
+        median_rate: "0.693210"
+      },
+      {
+        currency_code: "CHF",
+        buying_rate: "6.657305",
+        selling_rate: "6.697369",
+        unit_value: 1,
+        median_rate: "6.677337"
+      },
+      {
+        currency_code: "GBP",
+        buying_rate: "8.036510",
+        selling_rate: "8.084874",
+        unit_value: 1,
+        median_rate: "8.060692"
+      },
+      {
+        currency_code: "USD",
+        buying_rate: "6.595187",
+        selling_rate: "6.634877",
+        unit_value: 1,
+        median_rate: "6.615032"
+      },
+      {
+        currency_code: "BAM",
+        buying_rate: "3.758505",
+        selling_rate: "3.781123",
+        unit_value: 1,
+        median_rate: "3.769814"
+      },
+      {
+        currency_code: "EUR",
+        buying_rate: "7.350996",
+        selling_rate: "7.395234",
+        unit_value: 1,
+        median_rate: "7.373115"
+      },
+      {
+        currency_code: "PLN",
+        buying_rate: "1.713639",
+        selling_rate: "1.723951",
+        unit_value: 1,
+        median_rate: "1.718795"
+      }
+    ]
+  };
+
   render() {
-    return <div>To DO</div>;
+    return this.state.currencies.map(item => <div> {item.currency_code}</div>);
   }
 }
